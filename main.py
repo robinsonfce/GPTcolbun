a555ee61e7fc189b71aef9f144991010a27752aa
robinsonfce <robinson.fce@gmail.com>
2023-07-25 14:15:34 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,105 @@
+from langchain.chat_models import ChatOpenAI
+from langchain.chains import ConversationChain
+from langchain.chains.conversation.memory import ConversationBufferWindowMemory
+from langchain.prompts import (
+    SystemMessagePromptTemplate,
+    HumanMessagePromptTemplate,
+    ChatPromptTemplate,
+    MessagesPlaceholder
+)
+import streamlit as st
+from streamlit_chat import message
+from utils import *
+
+
+from dotenv import load_dotenv
+import os
+
+load_dotenv()  # Carga las variables de entorno desde .env
+
+OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY')
+PINECONE_API_KEY = os.environ.get('PINECONE_API_KEY')
+
+
+
+class Document:
+    def __init__(self, page_content, metadata):
+        self.page_content = page_content
+        self.metadata = metadata
+
+    def __repr__(self):
+        return f"Document(page_content='{self.page_content}', metadata={self.metadata})"
+
+    __str__ = __repr__
+
+
+st.markdown("<h2 style='text-align: center;'>Chatbot Colbun: Consulta lo que desees sobre seguridad y reglamentos de Colbun</h2>", unsafe_allow_html=True)
+
+st.markdown(
+    """
+    <img src="URL_A_TU_IMAGEN" width="100" align="middle">
+    """,
+    unsafe_allow_html=True,
+)
+
+st.markdown("<h3 style='text-align: center;'>Creado por: Robinson Cornejo</h3>", unsafe_allow_html=True)
+
+
+if 'responses' not in st.session_state:
+    st.session_state['responses'] = ["En que puedo ayudarte hoy"]
+
+if 'requests' not in st.session_state:
+    st.session_state['requests'] = []
+
+llm = ChatOpenAI(model_name="gpt-3.5-turbo", openai_api_key=OPENAI_API_KEY)
+
+if 'buffer_memory' not in st.session_state:
+            st.session_state.buffer_memory=ConversationBufferWindowMemory(k=3,return_messages=True)
+
+
+system_msg_template = SystemMessagePromptTemplate.from_template(
+    template="""Responda la pregunta con la mayor veracidad posible utilizando el contexto proporcionado,
+y si la respuesta no está contenida en el texto a continuación, diga 'Podrias preguntarle al equipo MASSO, seguramente ellos podran orientarte' """)
+
+
+human_msg_template = HumanMessagePromptTemplate.from_template(template="{input}")
+
+prompt_template = ChatPromptTemplate.from_messages([system_msg_template, MessagesPlaceholder(variable_name="history"), human_msg_template])
+
+conversation = ConversationChain(memory=st.session_state.buffer_memory, prompt=prompt_template, llm=llm, verbose=True)
+
+
+
+
+# container for chat history
+response_container = st.container()
+# container for text box
+textcontainer = st.container()
+
+
+with textcontainer:
+    query = st.text_input("Consulta: ", key="input")
+    if query:
+        with st.spinner("Escribiendo..."):
+            conversation_string = get_conversation_string()
+            st.code(conversation_string)
+            #user_entities = extract_entities(query)
+            #print("#######")
+            #print(user_entities)
+            refined_query = query_refiner(conversation_string, query)
+            #st.subheader("Refined Query:")
+            #st.write(refined_query)
+            context = get_answer(refined_query)
+            print(context)  
+            response = conversation.predict(input=f"Context:\n {context} \n\n Query:\n{query}")
+        st.session_state.requests.append(query)
+        st.session_state.responses.append(response) 
+with response_container:
+   if st.session_state['responses']:
+
+        for i in range(len(st.session_state['responses'])):
+            message(st.session_state['responses'][i],key=str(i))
+            if i < len(st.session_state['requests']):
+                message(st.session_state["requests"][i], is_user=True,key=str(i)+ '_user')
+
+          
---
+++ utils.py
@@ -0,0 +1,165 @@
+import pinecone
+import openai
+import streamlit as st
+
+from langchain.chains.question_answering import load_qa_chain
+from langchain.llms import OpenAI
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.chains.qa_with_sources import load_qa_with_sources_chain
+from langchain.prompts import PromptTemplate
+
+
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY')
+PINECONE_API_KEY = os.environ.get('PINECONE_API_KEY')
+
+#--------------------------------------------------------
+
+model_name = "text-embedding-ada-002"
+
+pinecone.init(      
+	api_key=PINECONE_API_KEY,      
+	environment='us-west4-gcp'      
+)      
+index = pinecone.Index('ric')
+index_pinecone_index          = pinecone.Index(index_name='ric')
+embeddings = OpenAIEmbeddings()
+#--------------------------------------------------------
+
+
+
+class Document:
+    def __init__(self, page_content, metadata):
+        self.page_content = page_content
+        self.metadata = metadata
+
+    def __repr__(self):
+        return f"Document(page_content='{self.page_content}', metadata={self.metadata})"
+
+    __str__ = __repr__
+
+def transform_dict_to_document(dict_list):
+    document_list = []
+    
+    for dict_obj in dict_list:
+        # Extraer el contenido de la página y los metadatos del diccionario
+        page_content = dict_obj['metadata']['text']
+        page_content = page_content.replace('\n', '')  # Elimina los saltos de línea
+
+        metadata = {'page': dict_obj['metadata']['page'], 'source': dict_obj['metadata']['source']}
+
+        # Crear un Document con el contenido de la página y los metadatos
+        doc = Document(page_content=page_content, metadata=metadata)
+
+        # Añadir el Document a la lista
+        document_list.append(doc)
+
+    # Devolver la lista de Documents
+    return document_list
+
+  
+
+def get_similiar_docs_pinecone(query,k=10,score=False):
+  import json
+  query_embedding= embeddings.embed_query(query)
+  result_query = index_pinecone_index.query(query_embedding, top_k=k, include_metadata=True)
+  result_query_json=json.dumps(result_query.to_dict())
+
+  def json_to_list(json_string):
+    # Convertir la cadena de caracteres a diccionario
+    json_string = json_string.replace("'", '"')  # JSON necesita comillas dobles
+    json_dict = json.loads(json_string)
+    
+    # Extraer 'matches' que es una lista de diccionarios
+    matches_list = json_dict['matches']
+
+    return matches_list
+
+  similar_docs=transform_dict_to_document(json_to_list(result_query_json))
+
+  return similar_docs
+
+##llm = OpenAI(model_name=model_name)
+##chain = load_qa_chain(llm, chain_type="stuff")
+
+
+initial_template = """
+You are an expert in occupational safety and protocols and you represent the company Colbun S.A.
+You should not repeat the question in your answers.
+Respond in the kindest way and with as much information as possible.
+If you do not know the answer, advise the user to consult their boss or a MASSO advisor.
+always explain your sources
+QUESTION: {question}
+=========
+{summaries}
+=========
+
+"""
+
+
+PROMPT = PromptTemplate(template=initial_template, input_variables=["summaries", "question"])
+
+
+llm = OpenAI(temperature=0.3, model_name="gpt-3.5-turbo-0301", max_tokens=512)
+qa = load_qa_with_sources_chain(llm=llm, chain_type="stuff", prompt=PROMPT)
+
+"""
+def get_answer(query):
+  similar_docs = get_similiar_docs_pinecone(query)
+  print(similar_docs)
+  print('------')
+  #answer = chain.run(input_documents=similar_docs, question=query)
+  answer =  qa({"input_documents": similar_docs, "question": query}, return_only_outputs=True)
+  return answer
+
+"""
+
+
+def get_answer(query):
+  similar_docs = get_similiar_docs_pinecone(query)
+  print(similar_docs)
+  print('------')
+  answer =  qa({"input_documents": similar_docs, "question": query}, return_only_outputs=True)
+  return answer
+
+
+def query_refiner(conversation, query):
+
+    response = openai.Completion.create(
+    model="text-davinci-003",
+    prompt=f"Dada la consulta del usuario y el historial de la conversación, tu objetivo es formular una pregunta más refinada y específica que te permita obtener la información más relevante de la base de conocimientos para responder de la mejor manera posible.\n\nCONVERSATION LOG: \n{conversation}\n\nQuery: {query}\n\nRefined Query:",
+    temperature=0.5,
+    max_tokens=512,
+    top_p=1,
+    frequency_penalty=0,
+    presence_penalty=0
+    )
+    return response['choices'][0]['text']
+
+"""
+def query_refiner(conversation, query):
+    user_entities = extract_entities(query)
+    refined_query = query + " " + " ".join(user_entities)
+    response = openai.Completion.create(
+    model="text-davinci-003",
+    prompt=f"Dada la consulta del usuario, las entidades identificadas y el historial de la conversación, tu objetivo es formular una pregunta más refinada y específica que te permita obtener la información más relevante de la base de conocimientos para responder de la mejor manera posible.\n\nCONVERSATION LOG: \n{conversation}\n\nQuery: {refined_query}\n\nRefined Query:",
+    temperature=0.5,
+    max_tokens=512,
+    top_p=1,
+    frequency_penalty=0,
+    presence_penalty=0
+    )
+    return response['choices'][0]['text']
+"""
+def get_conversation_string():
+    conversation_string = ""
+    for i in range(len(st.session_state['responses'])-1):
+        
+        conversation_string += "Human: "+st.session_state['requests'][i] + "\n"
+        conversation_string += "Bot: "+ st.session_state['responses'][i+1] + "\n"
+    return conversation_string
+
