0918440849ba2526c7f568dfbfe83d7ad2fb07d9
robinsonfce <robinson.fce@gmail.com>
2023-07-25 20:44:10 -0400

    Update main.py

--- main.py
+++ main.py
@@ -45,8 +45,7 @@ st.markdown(
 st.markdown("<h6 style='text-align: center;'>Creado por: Robinson Cornejo</h6>", unsafe_allow_html=True)
 
 
-if 'responses' not in st.session_state:
-    st.session_state['responses'] = ["En que puedo ayudarte hoy"]
+
 
 if 'requests' not in st.session_state:
     st.session_state['requests'] = []
