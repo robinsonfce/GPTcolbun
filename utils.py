cb72deba5eeb7ec0833b8ddb1274572f08727e8e
robinsonfce <robinson.fce@gmail.com>
2023-09-17 19:09:15 -0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,34 +1,27 @@
-import pinecone
+# Importaciones de servicios y bibliotecas
+import json
+import os
 import openai
+import pinecone
 import streamlit as st
-
-from langchain.chains.question_answering import load_qa_chain
+from dotenv import load_dotenv
+from langchain.chains.qa_with_sources import load_qa_with_sources_chain
 from langchain.llms import OpenAI
 from langchain.embeddings.openai import OpenAIEmbeddings
-from langchain.chains.qa_with_sources import load_qa_with_sources_chain
 from langchain.prompts import PromptTemplate
 
+# Configuración y constantes globales
+load_dotenv()
 
-import os
-from dotenv import load_dotenv
-
-# Define las variables de entorno y otros secrets
 OPENAI_API_KEY = st.secrets["OPENAI_API_KEY"]
 PINECONE_API_KEY = st.secrets["PINECONE_API_KEY"]
-#--------------------------------------------------------
 
-model_name = "text-embedding-ada-002"
+MODEL_NAME = "text-embedding-ada-002"
+pinecone.init(api_key=PINECONE_API_KEY, environment='us-west4-gcp')
+INDEX_PINECONE = pinecone.Index(index_name='codegpt')
+EMBEDDINGS = OpenAIEmbeddings()
 
-pinecone.init(      
-	api_key=PINECONE_API_KEY,      
-	environment='us-west4-gcp'      
-)      
-index = pinecone.Index('codegpt')
-embeddings = OpenAIEmbeddings()
-#--------------------------------------------------------
-
-
-
+# Clase para representar un documento
 class Document:
     def __init__(self, page_content, metadata):
         self.page_content = page_content
@@ -39,6 +32,7 @@ class Document:
 
     __str__ = __repr__
 
+# Función para transformar un diccionario en un Document
 def transform_dict_to_document(dict_list):
     document_list = []
     
@@ -80,11 +74,41 @@ def get_similiar_docs_pinecone(query,k=10,score=False):
 
   return similar_docs
 
-##llm = OpenAI(model_name=model_name)
-##chain = load_qa_chain(llm, chain_type="stuff")
 
 
-initial_template = """
+# Función para refinar una consulta dada una conversación anterior
+def query_refiner(conversation, query):
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=f"Dada la consulta del usuario y el historial de la conversación, tu objetivo es formular una pregunta más refinada y específica centrada en el área de normativas eléctricas. Esta pregunta refinada debe ayudarte a obtener la información más relevante de la base de conocimientos para responder de la mejor manera posible. La consulta refinada debe estar en forma de pregunta y no exceder de 2 oraciones.\n\nCONVERSATION LOG: \n{conversation}\n\nQuery: {query}\n\nRefined Query:",
+    	temperature=0.3,
+        max_tokens=512,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0
+    )
+    return response['choices'][0]['text']
+
+# Función para obtener el historial de conversación
+def get_conversation_string():
+    conversation_string = ""
+    for i in range(len(st.session_state['responses'])-1):
+        
+        conversation_string += "Human: "+st.session_state['requests'][i] + "\n"
+        conversation_string += "Bot: "+ st.session_state['responses'][i+1] + "\n"
+    return conversation_string
+
+
+# Función para obtener una respuesta a una consulta
+def get_answer(query):
+    similar_docs = get_similiar_docs_pinecone(query)
+    print(similar_docs)
+    print('------')
+    answer = qa({"input_documents": similar_docs, "question": query}, return_only_outputs=True)
+    return answer
+
+# Plantilla de aviso inicial
+INITIAL_TEMPLATE = """
 Eres un experto en normativas eléctricas, creado por Robinson Cornejo Evans
 No debes repetir la pregunta en tus respuestas.
 Responde de la forma más amable y con la mayor cantidad de información posible.
@@ -104,66 +128,11 @@ QUESTION: {question}
 =========
 """
 
+PROMPT = PromptTemplate(template=INITIAL_TEMPLATE, input_variables=["summaries", "question"])
 
-PROMPT = PromptTemplate(template=initial_template, input_variables=["summaries", "question"])
-
-
-llm = OpenAI(temperature=0.3, model_name="gpt-4", max_tokens=2048)
-qa = load_qa_with_sources_chain(llm=llm, chain_type="stuff", prompt=PROMPT)
-
-"""
-def get_answer(query):
-  similar_docs = get_similiar_docs_pinecone(query)
-  print(similar_docs)
-  print('------')
-  #answer = chain.run(input_documents=similar_docs, question=query)
-  answer =  qa({"input_documents": similar_docs, "question": query}, return_only_outputs=True)
-  return answer
-
-"""
+LLM = OpenAI(temperature=0.3, model_name="gpt-4", max_tokens=2048)
+QA = load_qa_with_sources_chain(llm=LLM, chain_type="stuff", prompt=PROMPT)
 
 
-def get_answer(query):
-  similar_docs = get_similiar_docs_pinecone(query)
-  print(similar_docs)
-  print('------')
-  answer =  qa({"input_documents": similar_docs, "question": query}, return_only_outputs=True)
-  return answer
 
-
-def query_refiner(conversation, query):
-
-    response = openai.Completion.create(
-    model="text-davinci-003",
-    prompt=f"Dada la consulta del usuario y el historial de la conversación, tu objetivo es formular una pregunta más refinada y específica centrada en el área de normativas eléctricas. Esta pregunta refinada debe ayudarte a obtener la información más relevante de la base de conocimientos para responder de la mejor manera posible. La consulta refinada debe estar en forma de pregunta y no exceder de 2 oraciones.\n\nCONVERSATION LOG: \n{conversation}\n\nQuery: {query}\n\nRefined Query:",
-    temperature=0.3,
-    max_tokens=512,
-    top_p=1,
-    frequency_penalty=0,
-    presence_penalty=0
-    )
-    return response['choices'][0]['text']
-
-"""
-def query_refiner(conversation, query):
-    user_entities = extract_entities(query)
-    refined_query = query + " " + " ".join(user_entities)
-    response = openai.Completion.create(
-    model="text-davinci-003",
-    prompt=f"Dada la consulta del usuario, las entidades identificadas y el historial de la conversación, tu objetivo es formular una pregunta más refinada y específica que te permita obtener la información más relevante de la base de conocimientos para responder de la mejor manera posible.\n\nCONVERSATION LOG: \n{conversation}\n\nQuery: {refined_query}\n\nRefined Query:",
-    temperature=0.5,
-    max_tokens=512,
-    top_p=1,
-    frequency_penalty=0,
-    presence_penalty=0
-    )
-    return response['choices'][0]['text']
-"""
-def get_conversation_string():
-    conversation_string = ""
-    for i in range(len(st.session_state['responses'])-1):
-        
-        conversation_string += "Human: "+st.session_state['requests'][i] + "\n"
-        conversation_string += "Bot: "+ st.session_state['responses'][i+1] + "\n"
-    return conversation_string
 
